d3311fcfeaa78e0ff7f07b77c6078631b792e28b
norbert-kulus-arianelabs <norbert.kulus@arianelabs.com>
2024-03-25 15:44:58 +0100

    (style): Remove pointer style of cursor on active tab list element
    Signed-off-by: norbert-kulus-arianelabs <norbert.kulus@arianelabs.com>

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -40,9 +40,9 @@ export const TabsListElement = ({ isActive, onClick, children }: { children: JSX
   <button
     type="button"
     className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+      'text-md inline-flex w-full cursor-pointer items-center justify-center whitespace-nowrap rounded-sm px-3 py-1 text-sm font-semibold ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
       {
-        'bg-background text-foreground shadow-sm': isActive,
+        'cursor-auto bg-background text-foreground shadow-sm': isActive,
       },
     )}
     onClick={onClick}
